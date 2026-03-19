c95f7da9ba4155140800b7889007bc388380637b
Garv <garvgandhi@gmail.com>
2026-03-19 13:51:26 +0530

    added request logs

--- warehouse-backend/index.js
+++ warehouse-backend/index.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Request logger
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/auth", authRoutes);
 app.use("/public", publicRoutes);
 app.use("/user", userRoutes);
